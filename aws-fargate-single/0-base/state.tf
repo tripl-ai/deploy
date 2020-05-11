26540db47786c16f72da0d1b0873558915d706ce
Yang <meloyang@f0189888f48a.ant.amazon.com>
2020-05-12 09:14:31 +1000

    upgrade remote state version

--- aws-fargate-single/0-base/state.tf
+++ aws-fargate-single/0-base/state.tf
@@ -11,7 +11,7 @@ data "aws_caller_identity" "current" {}
 
 # s3 bucket for tf remote state
 module "tf_remote_state" {
-  source = "github.com/turnerlabs/terraform-remote-state?ref=v2.0.0"
+  source = "github.com/turnerlabs/terraform-remote-state?ref=v3.0.0"
 
   role        = var.s3_read_access_role
   application = "${var.app}${data.aws_caller_identity.current.account_id}"
